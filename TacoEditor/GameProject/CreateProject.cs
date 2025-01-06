c51bc35b475868092260c150828237503940f7e7
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-01-06 21:16:34 +0800

    update

---
+++ TacoEditor/GameProject/CreateProject.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using TacoEditor.Utilities;
+
+namespace TacoEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember] public string ProjectType { get; set; }
+        [DataMember] public string ProjectFile { get; set; }
+        [DataMember] public List<string> Folders { get; set; }
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilPath { get; set; }
+    }
+
+    public class CreateProject : ViewModelBase
+    {
+        // TODO : Get Path from Installation
+        private readonly string _templatePath = @"..\..\TacoEditor\ProjectTemplates";
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set {
+                if(_projectName != value)
+                {
+                    _projectName = value;
+                    ValidateProjectPath();
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+        
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\TacoTruckProjects\"; 
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set {
+                if(_projectPath != value)
+                {
+                    _projectPath = value;
+                    ValidateProjectPath();
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+        private bool _isValid;
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                if (_errorMessage != value)
+                {
+                    _errorMessage = value;
+                    OnPropertyChanged(nameof(ErrorMessage));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path)) path += @"\";
+            path += $@"{ProjectName}\";
+
+            IsValid = false;
+            if (String.IsNullOrWhiteSpace(ProjectName.Trim()))
+            {
+                ErrorMessage = "Type in a project name.";
+            }
+            else if (ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMessage = "Invalid character(s) used in project name.";
+            }
+            else if (String.IsNullOrEmpty(ProjectPath.Trim()))
+            {
+                ErrorMessage = "Select a valid project folder.";
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMessage = "Invalid character(s) used in project path.";
+            }
+            else if (Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                ErrorMessage = "Selected project folder already exists and is not empty.";
+            }
+            else
+            {
+                ErrorMessage = string.Empty;
+                IsValid = true;
+            }
+
+            return IsValid;
+        }
+        public CreateProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "screenshot.jpg"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    _projectTemplates.Add(template);
+                }
+                ValidateProjectPath();
+            } 
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO : Log Error in own kind of log
+            }
+        }
+    }
+}
